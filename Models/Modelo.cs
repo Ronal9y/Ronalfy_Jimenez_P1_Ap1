fb2b3bb8b4a2b153b28c678dd61a9ebe15a31b98
DELL <DELL@DESKTOP-7VMN3MI>
2025-02-10 22:48:38 -0400

    Add project files.

---
+++ Models/Modelo.cs
@@ -0,0 +1,6 @@
+﻿namespace Ronalfy_Jimenez_P1_Ap1.Models
+{
+    public class Modelo
+    {
+    }
+}
---
+++ Services/Service.cs
@@ -0,0 +1,6 @@
+﻿namespace Ronalfy_Jimenez_P1_Ap1.Services
+{
+    public class Service
+    {
+    }
+}
