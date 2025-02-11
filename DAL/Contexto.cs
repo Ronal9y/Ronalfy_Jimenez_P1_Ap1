66eb5a6619b2cdd9f3ea591463753375fcf0148d
DELL <DELL@DESKTOP-7VMN3MI>
2025-02-10 23:03:15 -0400

    Creando el Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ronalfy_Jimenez_P1_Ap1.Models;
+using Ronalfy_Jimenez_P1_Ap1.DAL;
+using Ronalfy_Jimenez_P1_Ap1.Models;
+using System.Collections.Generic;
+
+namespace Ronalfy_Jimenez_P1_Ap1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+        public DbSet<Modelo> Modelos { get; set; }
+    }
