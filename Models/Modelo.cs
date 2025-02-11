96401331885c143dc771a519dafa6c8505bbcfd4
DELL <DELL@DESKTOP-7VMN3MI>
2025-02-10 22:49:55 -0400

    Creando la tabla Modelo

--- Models/Modelo.cs
+++ Models/Modelo.cs
@@ -1,6 +1,12 @@
-﻿namespace Ronalfy_Jimenez_P1_Ap1.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ronalfy_Jimenez_P1_Ap1.Models
 {
     public class Modelo
     {
+        [Key]
+        public int ModeloId { get; set; }
+
     }
 }
