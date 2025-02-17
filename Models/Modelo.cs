04d62607282f93de8468e09ade407e55a3e6ae58
DELL <DELL@DESKTOP-7VMN3MI>
2025-02-17 19:43:35 -0400

    Creando la tabla para la consulta de aportes

--- Models/Modelo.cs
+++ Models/Modelo.cs
@@ -6,7 +6,18 @@ namespace Ronalfy_Jimenez_P1_Ap1.Models
     public class Modelo
     {
         [Key]
-        public int ModeloId { get; set; }
+        public int AporteId { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir la fecha")]
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+        [Required(ErrorMessage = "Este campo obligatorio")]
+        public string? Persona { get; set; }
+        [Required(ErrorMessage = "Este campo obligatorio")]
+        public string? Observacion { get; set; }
+        [Required(ErrorMessage = "Este campo obligatorio")]
+        public decimal? Monto { get; set; }
+
 
     }
 }
